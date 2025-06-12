a53dd9f8af9e492ac72eadc292f83b60d0baddc0
divyanshkande <divyanshkande04@gmail.com>
2025-06-12 16:48:06 +0530

    Initial backend setup: Spring Boot + MySQL + User Entity

---
+++ src/main/java/com/example/finsage/FinsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.finsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsageApplication.class, args);
+	}
+
+}
